4d06b7971773d76f7996e47e71f116a074f706cc
Carlos Gonzales <carlos.gonzales.spo17@ecusolna.onmicrosoft.com>
2019-01-18 11:51:10 +0100

    Video

---
+++ VideoOnDemand.Data/Data/VODContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VideoOnDemand.Data.Entities;
+
+namespace VideoOnDemand.Data.Data
+{
+    public class VODContext:IdentityDbContext<User>
+    {
+        public VODContext(DbContextOptions<VODContext> options)
+        :base(options)
+        {           
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
